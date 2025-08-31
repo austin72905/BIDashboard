84fabddd5e46d0329e61e2e2ef1043ea3096f8e3
AUSTIN72905\Linpo <Linponggood@gmail.com>
2025-09-01 03:09:12 +0800

    取消cacelationtoken

--- Interfaces/IMetricService.cs
+++ Interfaces/IMetricService.cs
@@ -4,7 +4,7 @@ namespace BIDashboardBackend.Interfaces
 {
     public interface IMetricService
     {
-        Task<AgeDistributionDto> GetAgeDistributionAsync(long datasetId, CancellationToken ct = default);
-        Task<GenderShareDto> GetGenderShareAsync(long datasetId, CancellationToken ct = default);
+        Task<AgeDistributionDto> GetAgeDistributionAsync(long datasetId);
+        Task<GenderShareDto> GetGenderShareAsync(long datasetId);
     }
 }
