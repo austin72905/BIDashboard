fac34ed381cf5ba827379652ae787c2a2217ba6d
AUSTIN72905\Linpo <Linponggood@gmail.com>
2025-09-12 10:18:59 +0800

    update 反序列化也使用自訂轉換器

--- Utils/Json.cs
+++ Utils/Json.cs
@@ -14,17 +14,20 @@ namespace BIDashboardBackend.Utils
                 Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
             };
             options.Converters.Add(new DateTimeConverterUsingDateTimeParse(dateTimeFormat));
+            options.Converters.Add(new NullableDateTimeConverterUsingDateTimeParse(dateTimeFormat));
 
             return JsonSerializer.Serialize(dataObject, options);
         }
 
         // 將 JSON 字串反序列化為物件
-        public static T Deserialize<T>(string jsonString)
+        public static T Deserialize<T>(string jsonString, string dateTimeFormat = "yyyy-MM-dd HH:mm:ss")
         {
             var options = new JsonSerializerOptions
             {
                 PropertyNameCaseInsensitive = true
             };
+            options.Converters.Add(new DateTimeConverterUsingDateTimeParse(dateTimeFormat));
+            options.Converters.Add(new NullableDateTimeConverterUsingDateTimeParse(dateTimeFormat));
 
             return JsonSerializer.Deserialize<T>(jsonString, options);
         }
@@ -48,6 +51,49 @@ namespace BIDashboardBackend.Utils
         public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
         {
             writer.WriteStringValue(value.ToString(_dateTimeFormat));
+        }
+    }
+
+    // 自訂 Nullable<DateTime> 轉換器，使用指定的日期時間格式
+    public class NullableDateTimeConverterUsingDateTimeParse : JsonConverter<DateTime?>
+    {
+        private readonly string _dateTimeFormat;
+
+        public NullableDateTimeConverterUsingDateTimeParse(string dateTimeFormat)
+        {
+            _dateTimeFormat = dateTimeFormat;
+        }
+
+        public override DateTime? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.Null)
+            {
+                return null;
+            }
+
+            if (reader.TokenType == JsonTokenType.String)
+            {
+                var stringValue = reader.GetString();
+                if (string.IsNullOrEmpty(stringValue))
+                {
+                    return null;
+                }
+                return DateTime.ParseExact(stringValue, _dateTimeFormat, null);
+            }
+
+            throw new JsonException($"無法將 {reader.TokenType} 轉換為 DateTime?");
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime? value, JsonSerializerOptions options)
+        {
+            if (value.HasValue)
+            {
+                writer.WriteStringValue(value.Value.ToString(_dateTimeFormat));
+            }
+            else
+            {
+                writer.WriteNullValue();
+            }
         }
     }
 }
